a81c92ec1f6041ce23de64e7079fe3a006167578
Laura Pais <pais.laura@gmail.com>
2022-01-04 16:55:53 +0000

    title

---
+++ javaScript/title_emo.js
@@ -0,0 +1,47 @@
+var element_emo_1 =document.getElementById("intro_emo_1")
+var element_emo_2 =document.getElementById("intro_emo_2");
+var element_emo_3 =document.getElementById("intro_emo_3");
+var element_emo_4 =document.getElementById("intro_emo_4");
+var element_emo_5 =document.getElementById("intro_emo_5");
+var element_emo_6 =document.getElementById("intro_emo_6");
+
+
+function state1(){
+    element_emo_4.style.left="calc("+element_emo_3.clientWidth+"px + 2%)";
+    element_emo_4.style.transform = "translateX(0%)";
+
+    element_emo_5.style.left = "100%";
+    element_emo_5.style.transform = "translateX(-100%)";
+
+    element_emo_6.style.left="0";
+    element_emo_6.style.transform = "translateX(0%)";
+
+    setTimeout(state2, 2000);
+}
+
+function state2(){
+    element_emo_2.style.left="calc("+element_emo_1.clientWidth+"px + 2%)";
+    element_emo_2.style.transform = "translateX(0%)";
+
+    element_emo_4.style.left = "100%";
+    element_emo_4.style.transform = "translateX(-100%)";
+
+    element_emo_5.style.left = "100%";
+    element_emo_5.style.transform = "translateX(-100%)";
+
+    setTimeout(state3, 2000);
+}
+
+function state3(){
+    element_emo_2.style.left = "100%";
+    element_emo_2.style.transform = "translateX(-100%)";
+
+    element_emo_5.style.left="0";
+    element_emo_5.style.transform = "translateX(0%)";
+
+    element_emo_6.style.left = "100%";
+    element_emo_6.style.transform = "translateX(-100%)";
+
+    setTimeout(state1, 2000);
+}
+state1();
