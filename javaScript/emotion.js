83ec4e36a1278a2fc38672620a0a4553bd330677
Laura Pais <pais.laura@gmail.com>
2022-01-04 16:10:09 +0000

    +

--- javaScript/emotion.js
+++ javaScript/emotion.js
@@ -1,4 +1,5 @@
 var imagem=document.getElementsByClassName("emotion_animation");
+var imagem_2=document.getElementsByClassName("emotion_animation_2");
 
 for(var i=0; i<imagem.length; i++){
     imagem[i].active=Math.floor(getRandomNumber(1,4));
@@ -6,12 +7,25 @@ for(var i=0; i<imagem.length; i++){
 }
 
 
+for(var i=0; i<imagem_2.length; i++) {
+    imagem_2[i].active = Math.floor(getRandomNumber(1, 4));
+    setInterval(animation_2, 200, imagem_2[i]);
+}
+
 function animation(emotion){
     if (emotion.active < 4)
         emotion.active++;
     else
         emotion.active = 1;
     emotion.src = "data/img/fotos_artigo/" + emotion.dataset.src + "." + emotion.active + ".png";
+}
+
+function animation_2(emotion){
+    if (emotion.active < 4)
+        emotion.active++;
+    else
+        emotion.active = 1;
+    emotion.src = "data/img/fotos_artigo/" + emotion.dataset.src + "." + emotion.active + ".svg";
 }
 
 function getRandomNumber(min, max) {
