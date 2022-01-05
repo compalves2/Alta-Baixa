c803fba4f8a3d43a53abb7985a92913b39d78f90
Laura Pais <pais.laura@gmail.com>
2022-01-05 19:58:14 +0000

    SETAAAA

--- javaScript/seta.js
+++ javaScript/seta.js
@@ -1,17 +1,13 @@
 var image1 =  document.querySelector("#img_seta>img");
 var janela =  document.getElementById("artigo_typeEm");
-//image1.style = "position: absolute; width: 700px; height: 700px; margin-left: 40%;"
-var rect1 = image1.getBoundingClientRect();
-var centerX = janela.clientWidth/2;
-var centerY = janela.clientHeight/2;
-
 
 document.onmousemove = function(e) {
+  var rect1 = image1.getBoundingClientRect();
+  var centerX = rect1.left + rect1.width * 0.5-image1.clientWidth/2;
+  var centerY = (rect1.top + rect1.height * 0.5);
   var dx = e.pageX - centerX;
   var dy = e.pageY - centerY;
   var theta = Math.atan2(dy, dx);
-  var pi = Math.PI;
-  var angle = theta * (180/pi);
-  var trueangle = angle;
-  //image1.style.transform = `rotate(${trueangle}deg) translate(-50%, 0%)`;
+  var angle = theta * (180/Math.PI);
+  image1.style.transform = `translate(-50%, 0%) rotate(${45+angle}deg)`;
 };
