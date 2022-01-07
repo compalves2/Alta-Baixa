62ad27d824e91f95fcb6d815f9b9e68370a82624
Laura Pais <pais.laura@gmail.com>
2022-01-07 00:58:28 +0000

    top

--- javaScript/geral.js
+++ javaScript/geral.js
@@ -1,6 +1,12 @@
 var offsetStart = 0;
 var offsetEnd = 0;
 
+var artigo_typeEm = document.getElementById("artigo_typeEm");
+
 window.addEventListener('scroll', () => {
     document.documentElement.style.setProperty('--scroll', ( window.pageYOffset - offsetStart ) / ( document.body.offsetHeight - offsetStart - offsetEnd - window.innerHeight ));
 }, false);
+
+document.getElementById("goTopTypem").addEventListener("click", function (){
+    artigo_typeEm.scrollTop = 0;
+});
