6496bb71f4b5a5b0e7a3e850bf7f2dcf43dc01e4
Laura Pais <pais.laura@gmail.com>
2022-01-08 19:43:12 +0000

    +

--- javaScript/page.js
+++ javaScript/page.js
@@ -1,4 +1,5 @@
 var side_num = document.getElementsByClassName("pageSelect");
+var num_footer = document.getElementsByClassName("num_footer");
 var artigos = document.getElementsByClassName("artigo");
 
 var currentPage = -1;
@@ -9,21 +10,36 @@ for (var i = 0; i < side_num.length; i++) {
     side_num[i].addEventListener("click", goPage);
 }
 
+for (var i = 0; i < num_footer.length; i++) {
+    for (var j = 0; j < num_footer[i].children.length; j++) {
+        num_footer[i].children[j].number = j + 1;
+        num_footer[i].children[j].page = artigos[j];
+        num_footer[i].children[j].addEventListener("click", goPage);
+    }
+}
+
 function goPage() {
-    if(currentPage==-1) {
+    if (currentPage > 0) {
+        if (side_num[currentPage].classList.contains("active")) {
+            side_num[currentPage].classList.remove("active");
+        }
+    }
+    if (this.number > 0) {
+        side_num[this.number].classList.add("active");
+    }
+
+    if (currentPage == -1) {
         cubeRot();
     }
 
-    if(artigotypeEm.classList.contains("open")) {
+    if (artigotypeEm.classList.contains("open")) {
         artigotypeEm.classList.remove("open");
     }
 
-    if(this.number==0 && currentPage==0) {
+    if (this.number == 0 && currentPage == 0) {
         currentPage = -1;
         cubeRot();
-    }
-
-    else {
+    } else {
         currentPage = this.number;
         if (currentPage == 14) {
             cubeRot();
@@ -36,7 +52,7 @@ function goPage() {
                 side_num[i].page.classList.remove("close");
             }
         } else {
-            if (i!=side_num.length-1 && !side_num[i].page.classList.contains("close")) {
+            if (i != side_num.length - 1 && !side_num[i].page.classList.contains("close")) {
                 side_num[i].page.classList.add("close");
             }
         }
@@ -47,14 +63,14 @@ var topSection = document.getElementById("topSection");
 var artigotypeEm = document.getElementById("artigo_typeEm");
 
 function topPage() {
-    if(artigotypeEm.classList.contains("open")) {
+    if (artigotypeEm.classList.contains("open")) {
         artigotypeEm.classList.remove("open");
     } else {
         artigotypeEm.classList.add("open");
     }
 
     for (var i = 0; i < side_num.length; i++) {
-        if (i!=side_num.length-1  && !side_num[i].page.classList.contains("close")) {
+        if (i != side_num.length - 1 && !side_num[i].page.classList.contains("close")) {
             side_num[i].page.classList.add("close");
         }
     }
